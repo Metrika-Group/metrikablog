f799f2b15548b9e265e924c24220e2cfc7436b07
omarbenites <omar_cs_7@hotmail.com>
2021-09-02 19:14:31 -0500

    initial configuration

---
+++ main_routines.R
@@ -0,0 +1,8 @@
+#Publish in Github
+library(distill)
+file.create(".nojekyll")
+
+#ejemplo de como cambiar post
+rename_post_dir(post_dir = "_posts/2021-08-10-nuevo/",slug="R+Mobile Data Collection", "2021-08-25") 
+
+
